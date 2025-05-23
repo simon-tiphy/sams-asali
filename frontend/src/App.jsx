f6f58b6ceee278b71f68789b855e669fb1392df6
simon tiphy <simontiphy@gmail.com>
2025-05-24 02:36:25 +0300

    change to vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const App = () => {
+  return (
+    <div className='bg-red-500'>
+      <h1>testing 123</h1>
+    </div>
+  )
+}
+
+export default App
